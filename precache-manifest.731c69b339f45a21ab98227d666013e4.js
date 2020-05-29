2365af9b7e4f59362af87847b03df223570bad80
Oscar <oscar.ramos@ucsp.edu.pe>
2020-05-28 19:51:22 -0500

    Updates

---
+++ precache-manifest.731c69b339f45a21ab98227d666013e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7192130d6a0918eb989eeb26a6ee9271",
+    "url": "/playfair/index.html"
+  },
+  {
+    "revision": "51ac2bdc4ed9b36c2b3e",
+    "url": "/playfair/static/js/2.7140bb29.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/playfair/static/js/2.7140bb29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536b750e207261a87366",
+    "url": "/playfair/static/js/main.06b49d1a.chunk.js"
+  },
+  {
+    "revision": "bb46ddf1f6bca1983228",
+    "url": "/playfair/static/js/runtime-main.ecf03568.js"
+  }
+]);
